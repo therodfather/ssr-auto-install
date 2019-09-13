f4f9994e063eccaf3bedc00538bde68bfd91fe42
McSwanky <detorr@live.com>
2019-09-13 15:03:20 +0800

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -2,17 +2,21 @@
 # script for ubuntu 19 clean server for ssr and random password
 
 # update and install dependencies upgrade machine and clean
-#sudo apt-get install git tmux docker docker-compose -y;
-#sudo apt-get upgrade -y && apt-get dist-upgrade -y; 
-#sudo apt-get autoremove -y;
+sudo apt-get update;
+sudo apt-get purge docker docker-compose docker.io -y;
+sudo apt-get install git docker.io docker docker-compose -y;
+sudo apt-get upgrade -y && apt-get dist-upgrade -y; 
+sudo apt-get autoremove -y;
 
 #randomize password and replace in config file and wait some time to look at the config change
 #STR = date | md5sum;
-#sed 's/# "password": "p@ssw0rd",/"password": "$STR"/' ect/config.json;
+#sed 's/# "password": "p@ssw0rd",/"password": "$STR"/' etc/config.json;
+
 cat etc/config.json;
 process_id=$!;
 wait $process_id;
 
 # start up ssr from docker-compose.yml config file and run in background
-docker-compose up -d
+docker-compose up -d;
 
+echo "Dockies and Sockies install completed!!! ";
